a8d826edb4c709c2d16ab95ea384ab0310814bbd
Pablo Pino <pdpino@uc.cl>
2020-06-19 13:34:31 -0400

    Support teacher forcing

--- mrg/models/report_generation/cnn_to_seq.py
+++ mrg/models/report_generation/cnn_to_seq.py
@@ -14,11 +14,13 @@ class CNN2Seq(nn.Module):
         self.linear = nn.Linear(cnn.features_size, decoder.hidden_size)
         self.decoder = decoder
 
-    def forward(self, images, max_sentence_len):
+    def forward(self, images, max_sentence_len, reports=None):
         """Forward method.
 
         images -- shape: batch_size, 3, height, width
             (3 for RGB)
+        max_sentence_len -- int
+        reports -- shape: batch_size, max_sentence_len
         """
         features = self.cnn.features(images)
         # shape: batch_size, n_features, height, width
@@ -30,7 +32,7 @@ class CNN2Seq(nn.Module):
         initial_decoder_state = self.linear(features)
         # shape: batch_size, hidden_size
 
-        decoded, = self.decoder(initial_decoder_state, max_sentence_len)
+        decoded, = self.decoder(initial_decoder_state, max_sentence_len, reports=reports)
         # shape: batch_size, max_sentence_len, vocab_size
 
         return decoded,
--- mrg/models/report_generation/decoder_lstm.py
+++ mrg/models/report_generation/decoder_lstm.py
@@ -5,7 +5,7 @@ from mrg.utils import PAD_IDX, START_IDX
 
 
 class LSTMDecoder(nn.Module):
-    def __init__(self, vocab_size, embedding_size, hidden_size):
+    def __init__(self, vocab_size, embedding_size, hidden_size, teacher_forcing=True):
         super().__init__()
 
         self.hidden_size = hidden_size
@@ -14,11 +14,10 @@ class LSTMDecoder(nn.Module):
         self.embeddings_table = nn.Embedding(vocab_size, embedding_size, padding_idx=PAD_IDX)
         self.lstm_cell = torch.nn.LSTMCell(embedding_size, hidden_size)
         self.W_vocab = torch.nn.Linear(hidden_size, vocab_size)
+        
+        self.teacher_forcing = teacher_forcing
 
-    def forward(self,
-                initial_state,
-                max_sentence_length,
-                ):
+    def forward(self, initial_state, max_sentence_length, reports=None):
         batch_size, hidden_size = initial_state.size()
 
         # Build initial state
@@ -27,23 +26,35 @@ class LSTMDecoder(nn.Module):
         state = (initial_state, initial_c_state)
 
         # Build initial input
-        start_idx = self.start_idx.to(device).repeat(batch_size)
+        start_idx = self.start_idx.to(device).repeat(batch_size) # shape: batch_size
         input_t = self.embeddings_table(start_idx)
+        # shape: batch_size, embedding_size
 
+        # Decide teacher forcing
+        teacher_forcing = self.teacher_forcing and self.training and reports is not None
+        
         # Generate word by word
         seq_out = []
 
-        for i in range(max_sentence_length):
+        for word_i in range(max_sentence_length):
             # Pass thru LSTM
             state = self.lstm_cell(input_t, state)
             h_t, c_t = state
+            # shapes: batch_size, hidden_size
 
             # Predict with FC
-            prediction_t = self.W_vocab(h_t)
+            prediction_t = self.W_vocab(h_t) # shape: batch_size, vocab_size
             seq_out.append(prediction_t)
 
             # Get input for next word
-            _, max_indices = prediction_t.max(dim=1)
-            input_t = self.embeddings_table(max_indices)
+            if teacher_forcing:
+                next_words_indices = reports[:, word_i]
+                # shape: batch_size
+            else:
+                _, next_words_indices = prediction_t.max(dim=1)
+                # shape: batch_size
+
+            input_t = self.embeddings_table(next_words_indices)
+            # shape: batch_size, embedding_size
 
         return torch.stack(seq_out, dim=1),
