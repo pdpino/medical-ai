b5ff6aeef6756b17da56507735cbc9543923bcb6
Pablo Pino <pdpino@uc.cl>
2021-10-12 16:33:52 -0300

    Improve radgraph: plot by conn-components

--- medai/datasets/radgraph.py
+++ medai/datasets/radgraph.py
@@ -1,5 +1,7 @@
+# import math
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 _SHORTEN_RELATION = {
     'modify': 'M',
@@ -30,7 +32,7 @@ def print_id_to_tokens(graph, nodelist=None):
     nodelist = nodelist or list(graph)
     nodes_data = graph.nodes.data()
 
-    for node in nodelist:
+    for node in sorted(nodelist, key=int):
         info = nodes_data[node]
         print(f'{node}: {info["tokens"]} ({info["label"]})')
 
@@ -53,31 +55,67 @@ def _get_plotable_edges(graph):
         plotable_edges[(node_1, node_2)] = rel_type
     return plotable_edges
 
-def _conn_components_layout(graph):
-    # TODO: use connected-components to provide a better layout
-    # (manually space them apart)
-    for s in nx.algorithms.components.connected_components(graph.to_undirected()):
-        print(s)
-    raise NotImplementedError
+def _get_plotable_labels(graph):
+    nodes_data = graph.nodes.data()
+    d = dict()
+    for node in list(graph):
+        info = nodes_data[node]
+        d[node] = f'{info["tokens"]}'
+    return d
 
-def plot_radgraph(graph, figsize=(15, 8), layout='planar'):
+def _conn_components_layout(graph, n_cols=3, n_sep=2.5, sublayout='planar'):
+    sublayout_fn = nx.planar_layout if sublayout == 'planar' else nx.spring_layout
+
+    # Calculate layouts for each subgraph
+    subgraphs = [
+        sublayout_fn(subgraph)
+        for subgraph in nx.algorithms.components.connected_components(graph.to_undirected())
+    ]
+
+    # Sort them by number of nodes
+    subgraphs = sorted(subgraphs, key=len, reverse=True)
+
+    # Define number of rows and cols
+    # n_rows = math.ceil(len(subgraphs) / n_cols)
+
+    # New pos dict
+    pos = {}
+    for i, s in enumerate(subgraphs):
+        row = i // n_cols
+        col = i % n_cols
+        x_factor = col * n_sep
+        y_factor = row * n_sep
+        factor = np.array([x_factor, y_factor])
+        for key, value in s.items():
+            pos[key] = value + factor
+
+    return pos
+
+def plot_radgraph(graph, figsize=(15, 8), layout=None, labels=False, **kwargs):
     assert isinstance(graph, nx.Graph), f'got {type(graph)}'
 
     plt.figure(figsize=figsize)
     if layout == 'planar':
         pos = nx.planar_layout(graph)
-    else:
+    elif layout == 'random':
         pos = nx.spring_layout(graph, k=1)
+    else:
+        pos = _conn_components_layout(graph, **kwargs)
 
     nodelist, node_color = _get_plot_node_colors(graph)
     plotable_edges = _get_plotable_edges(graph)
 
     nx.draw_networkx(
         graph, pos,
-        with_labels=True, node_size=500,
+        with_labels=not labels, node_size=500,
         nodelist=nodelist, node_color=node_color,
     )
     nx.draw_networkx_edge_labels(
         graph, pos,
         edge_labels=plotable_edges, font_size=12,
     )
+    if labels:
+        nx.draw_networkx_labels(
+            graph, pos,
+            labels=_get_plotable_labels(graph),
+        )
